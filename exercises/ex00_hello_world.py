a0ab10afc66aee2efb56bdcdaa080f1fd3da91f4
gemallo <gemallo@ad.unc.edu>
2025-01-12 14:12:57 -0500

    Finished EX 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,1 +1,12 @@
-print("Hello, world!")
+"""My first exercise in COMP110!"""
+
+__author__ = "730468661"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+if __name__ == "__main__":
+    print(greet(name=input("What is your name? ")))
