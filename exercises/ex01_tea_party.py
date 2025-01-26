9509574361e8900e12c8c80d447d66316ecd62b9
gemallo <gemallo@ad.unc.edu>
2025-01-26 15:19:24 -0500

    tea party finsihed!

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -3,9 +3,17 @@
 __author__: str = "730468661"
 
 
-def main_planner(guests: int) -> none:
+def main_planner(guests: int) -> None:
     """Main function for planning tea party"""
-    return none
+    print("A Cozy Tea Party for " + str(guests) + " People!")
+    print("Tea Bags: " + str(tea_bags(people=guests)))
+    print("Treats: " + str(treats(people=guests)))
+    print(
+        "Cost: $"
+        + str(
+            cost(tea_count=tea_bags(people=guests), treat_count=treats(people=guests))
+        )
+    )
 
 
 def tea_bags(people: int) -> int:
@@ -15,9 +23,13 @@ def tea_bags(people: int) -> int:
 
 def treats(people: int) -> int:
     """Calculates how many treats are needed"""
-    return int(1.5 * tea_bags(people))
+    return int(1.5 * tea_bags(people=people))
 
 
 def cost(tea_count: int, treat_count: int) -> float:
     """Calculates the cost of the tea party based on number of tea bags and treats needed"""
     return 0.5 * tea_count + 0.75 * treat_count
+
+
+if __name__ == "__main__":
+    main_planner(guests=int(input("How many guests are attending your tea party? ")))
